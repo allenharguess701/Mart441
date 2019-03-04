91fc8f4bdeb2970ef07c64fe43a483c9d590c38e
Allen Harguess <allen.harguess@umconnect.umt.edu>
2019-03-04 01:28:51 -0700

    HW-7 push

---
+++ HW-7/README.md
@@ -0,0 +1,10 @@
+# Mart441
+# Homework Assignment 7
+## Allen Harguess
+
+The viewable version of this assignment is at [HW-7](https://allenharguess701.github.io/Mart441/HW-7/)
+
+### Work Flow
+So like before I added onto the existing website and as before the secondary navigation bar listing the homework assignments has the Picture Viewer link.
+
+I added in several images from the online game World of Warcraft. Specifically the Battle for Azeroth Expansion. I had issues with the image url coming up scrambled. It had something to do with the \ causing an issue so I set the url into a different array.
---
+++ HW-7/scripts/picture_viewer.js
@@ -0,0 +1,79 @@
+var picArray = new Array();
+var randnum = Math.floor((Math.random() * 10) + 1);
+var pictureurl = ['one.jpg', 'two.jpg', 'three.jpg', 'four.jpg', 'five.jpg', 'six.jpg', 'seven.jpg', 'eight.jpg', 'nine.jpg', 'ten.png'];
+
+class Picture
+{
+  constructor( title, desc, author, date )
+    {
+      this.title = title;
+      this.image = image;
+      this.desc = desc;
+      this.author = author;
+      this.date = date;
+    }
+
+    toStringTitle()
+    {
+        return "Title: " + this.title;    
+        
+    }
+	
+    toStringDesc()
+    {
+        return "Description: " + this.desc;
+
+    }
+		   
+	toStringAuthor()
+    {
+        return "Author: " + this.author;
+
+    }
+
+    toStringDate()
+    {
+        return "Year: " + this.date;
+
+	}
+}
+
+function initializeArray(){
+	  let one = new Picture("The Alliance", "This is a picture of the leaders of the Alliance Faction.", "Blizard Entertainment", "2018")
+	  let two = new Picture("The Horde", "This is a picture of the leaders of the Horde Faction.", "Blizard Entertainment", "2018")
+	  let three = new Picture("Good Vs Evil", "The load screen for the game itself featuring the two main protagonists.", "Blizard Entertainment", "2018")
+	  let four = new Picture("Alliance Banner", "The Alliance banner created by an unknown artist in 2015.", "Unknown", "2015")
+	  let five = new Picture ("Sylvanas Vs Anduin", "This is a picture from the cinematic featuring a clash between the leaders of both factions.", "Vanthromhre", "2018")
+	  let six = new Picture("Allied Races", "The new allied races who are available for play once you unlock them.", "Foosh-ball", "2017")
+	  let seven = new Picture("Sylvanas", "Lady Sylvanas Windrunner also referred to as the Dark Lady and the Banshee Queen is the Warchief of the Horde and supreme ruler of the Forsaken.", "Blizard Entertainment", "2018")
+	  let eight= new Picture("Anduin", "Anduin Llane Wrynn is the King of Stormwind, High King of the Alliance, and commander of all Alliance forces since the death of his father, Varian Wrynn.", "Bubba Hoptep", "2018")
+	  let nine = new Picture("Horde Banner", "The Horde banner created by an unknown artist in 2015.", "Unknown", "2015")
+	  let ten = new Picture ("Battle for Azeroth Logo", "The Battle for Azeroth Logo.", "Blizard Entertainment", "2017")
+
+	  picArray.push(one);
+	  picArray.push(two);
+	  picArray.push(three);
+	  picArray.push(four);
+	  picArray.push(five);
+	  picArray.push(six);
+	  picArray.push(seven);
+	  picArray.push(eight);
+	  picArray.push(nine);
+	  picArray.push(ten);
+
+	  console.log(one);
+	  console.log(two);
+
+}
+
+function accessinfo()
+{
+    var randnum = Math.floor((Math.random() * 10) + 1);
+	
+	document.getElementById("picurl").src = pictureurl[randnum];
+	console.log(pictureurl[randnum]);
+    document.getElementById("title").innerHTML = picArray[randnum].toStringTitle();	    
+    document.getElementById("desc").innerHTML = picArray[randnum].toStringDesc();
+	document.getElementById("author").innerHTML = picArray[randnum].toStringAuthor();
+    document.getElementById("date").innerHTML = picArray[randnum].toStringDate();
+}
